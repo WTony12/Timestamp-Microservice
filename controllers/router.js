59758754b9b1f4af50e49b200c4ba37a6011c2e7
WTony12 <wagdianthony@gmail.com>
2017-06-03 18:07:30 -0300

    ready to develop microstamp module

--- controllers/router.js
+++ controllers/router.js
@@ -1,10 +1,7 @@
 module.exports = function(app){
     //Route to root which renders index.html
-    app.get("/", function(err, req, res){
-        if(err)
-            console.error(err);
-        else
-            res.status(200).sendFile(__dirname + "/public/index.html");
+    app.get("/:time", function(req, res){
+        res.status(200).json({time: req.params.time});
     });
     //Send 404 Message incase of wrong route
     app.use(function(req, res, next){
